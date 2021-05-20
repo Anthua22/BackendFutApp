e7436055e1c93a5819979d6437984d40a8e4f901
Anthua22 <anthonyemmanuel.ubillus@iesdoctorbalmis.com>
2021-05-20 14:08:47 +0200

    intneto ver si pasa lago al crear una imagen

--- utils/uploads.js
+++ utils/uploads.js
@@ -1,9 +1,15 @@
 const base64ToImage = require('base64-to-image');
 
 let storage = (base64, carpeta) => {
-    const path = __dirname + './../uploads/images/' + carpeta + '/';
-    let optionalObj = { 'fileName': Date.now() + "_" };
-    return base64ToImage(base64, path, optionalObj);
+    try {
+        const path = __dirname + './../uploads/images/' + carpeta + '/';
+        let optionalObj = { 'fileName': Date.now() + "_" };
+        return base64ToImage(base64, path, optionalObj);
+
+    } catch (err) {
+        console.log(err)
+    }
+
 }
 
 

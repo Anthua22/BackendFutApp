06398376765b30ab99ee54f903fd9beb82416d38
Anthua22 <anthonyemmanuel.ubillus@iesdoctorbalmis.com>
2021-05-20 15:16:41 +0200

    termino las modificaciones de las imagenes

--- utils/uploads.js
+++ utils/uploads.js
@@ -1,4 +1,4 @@
-const { exception } = require('console');
+
 const fs = require('fs');
 
 let storage = (base64, carpeta) => {
@@ -6,7 +6,7 @@ let storage = (base64, carpeta) => {
     const extencionBlock = Image[0].split('/');
     const extencionSubBlock = extencionBlock[1].split(';');
     const name = Date.now() + '.' + extencionSubBlock[0];
-    const path = __dirname + './../uploads/images/' + carpeta + '/' + name;
+    const path = __dirname + '/../uploads/images/' + carpeta + '/' + name;
     fs.writeFile(path, Image[1], 'base64', function (err) {
         if (err) {
             throw 'No se ha podido crear el archivo';

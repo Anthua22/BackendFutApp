cc3a290fc5dd038457efd1e4adc7318533f92873
Anthony Ubillus <anthonyemmanuel.ubillus@iesdoctorbalmis.com>
2021-05-24 20:26:02 +0200

    añado cors manual

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ const corsOptions = {
     }
 }
 
+
+app.use(autenticado.peticionesLocalHost);
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb' }));
 app.use(express.static(__dirname + '/uploads/images'));
